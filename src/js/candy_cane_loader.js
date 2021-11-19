4fc5b86cea9b988a977ac8e698a51aafa37b84fb
Labhrás Mac Gabhann <coding@larrymg.me>
2021-11-19 14:08:37 -0500

    test: Added console.log messages to Ctrack progress of Candy Cane Loader event listener.

--- src/js/candy_cane_loader.js
+++ src/js/candy_cane_loader.js
@@ -1,4 +1,7 @@
-window.addEventListener("load", function (){
-    const loader = document.querySelector(".loader--cover");
-    loader.className += " loader--hidden";
-})
+window.addEventListener("load", function () {
+  console.log("Entering Candy Cane Loader event listener.");
+  const loader = document.querySelector(".loader--cover");
+  console.log("Attempting to remove Candy Lane Loader...");
+  loader.className += " loader--hidden";
+  console.log("Removed Candy Cane Loader.");
+});
